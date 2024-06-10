2da860916fe1e04fe64cd120ac6b4904bb5647fb
AlfarizaRasendria <rasendria.alfariza18@gmail.com>
2024-06-10 15:00:48 +0700

    Fixing Prisma Schema

--- prisma/migrations/20240603140048_remove_token/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `token` on the `refresh_session` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `refresh_session` DROP COLUMN `token`;
